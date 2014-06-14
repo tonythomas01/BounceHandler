a6beaa02ae527bc9610c99180c3db379ad4aafc7
Tony Thomas <01tonythomas@gmail.com>
2014-06-14 16:15:17 +0530

    Removed extra file.

--- BounceHandler.hooks.php
+++ BounceHandler.hooks.php
@@ -4,8 +4,29 @@
 
 class BounceHandlerHooks { 
 
-	public static function onVERPAddressGenerate( )	{
-
-		return true;
+	public static function onVERPAddressGenerate( $to, &$from )	{
+		echo "hi";
+		$from->address = self::generateVERP( $to->address );
+		return 	true;
+	}
+	/**
+	 * Generate VERP address
+	 *
+	 * @param $to
+	 *
+	 * @return ReturnPath address
+	 */
+	protected static function generateVERP( $to ) {
+		global $wgVERPalgo, $wgVERPsecret, $wgServer, $wgSMTP;
+		if(  is_array( $wgSMTP ) && isset( $wgSMTP['IDHost'] ) && $wgSMTP['IDHost'] ) {
+			$email_domain = $wgSMTP['IDHost'];
+		} else {
+			$url = wfParseUrl( $wgServer );
+			$email_domain = $url['host'];
+		}
+		$verp_hash = hash_hmac( $wgVERPalgo, $to, $wgVERPsecret );
+		$email_prefix = 'bounces';
+		$returnPath = $email_prefix.'-'.$verp_hash.'@'.$email_domain;
+		return $returnPath;
 	}
 }
