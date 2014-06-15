3968ac450b7e79e30116929f5ab53b520232199f
Tony Thomas <01tonythomas@gmail.com>
2014-06-15 15:14:44 +0530

    Add maintenance script

--- BounceHandler.php
+++ BounceHandler.php
@@ -31,3 +31,20 @@ $wgHooks['VERPAddressGenerate'][] = 'BounceHandlerHooks::onVERPAddressGenerate';
 
 # Schema updates for update.php
 $wgHooks['LoadExtensionSchemaUpdates'][] = 'BounceHandlerHooks::AddTable';
+
+
+/**
+ * VERP Configurations
+ * wgEnableVERP - Engales VERP for bounce handling
+ * wgVERPalgo - Algorithm to hash the return path address.Possible algorithms are
+ * md2. md4, md5, sha1, sha224, sha256, sha384, ripemd128, ripemd160, whirlpool and more.
+ * wgVERPsecret - The secret key to hash the return path address
+ */
+$wgEnableVERP = true;
+$wgVERPalgo = 'md5';
+$wgVERPsecret = 'MediaiwkiVERP';
+
+/* IMAP configs */
+$wgIMAPuser = 'user';
+$wgIMAPpass = 'pass';
+$wgIMAPserver = '{localhost:143/novalidate-cert}';
---
+++ maintenance.php
@@ -0,0 +1,34 @@
+<?php 
+require_once ( dirname(__FILE__). "/../../maintenance/Maintenance.php");
+class BounceHandlerClearance extends Maintenance {
+	public function __construct() { 
+		parent::__construct();
+		$this->mDescription = "Connect to IMAP server and take action on bounces";
+		$this->addArg( "imapuser", "IMAP account Username", false );
+		$this->addArg( "imappass", "IMAP account Password", false );
+	}
+	public function execute() {
+		global $wgServer, $wgIMAPuser, $wgIMAPpass, $wgIMAPserver;
+		echo $wgIMAPserver;
+		$imapuser = $this->getArg( 0 );
+		$imappass = $this->getArg( 1 );
+		if ( !is_object( $imapuser ) && ( $wgIMAPuser === null) ) {
+			$this->error( "invalid IMAP username.", true );
+		}
+		if ( !is_object( $imappass ) && ( $wgIMAPpass === null )) {
+			$this->error( "invalid IMAP password.", true );
+		}
+		if ( $wgIMAPserver === null ) {
+			$this->error( "invalid IMAP server.", true );
+		}
+		$conn = imap_open( $wgIMAPServer, $wgIMAPuser, $wgIMAPpass ) or die( imap_last_error() );
+		$num_msgs = imap_num_recent( $conn );
+		echo $num_msgs();
+	}
+}
+$maintClass = 'BounceHandlerClearance';
+if( defined('RUN_MAINTENANCE_IF_MAIN') ) {
+  require_once( RUN_MAINTENANCE_IF_MAIN );
+} else {
+  require_once( DO_MAINTENANCE ); # Make this work on versions before 1.17
+}
